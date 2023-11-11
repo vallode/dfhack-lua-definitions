650083c2efdf2c6d3ac9fe17dc477969c536a99a
vallode <18506096+vallode@users.noreply.github.com>
2023-11-11 09:50:28 +0100

    Clean empty text nodes before parsing

--- src/generate.rb
+++ src/generate.rb
@@ -19,35 +19,32 @@ Dir.glob(ARGV[0]).each do |xml|
   print "Parsing: #{xml}\n"
   document = Nokogiri::XML(File.open(xml), &:noblanks)
 
+  # Provided files have a single `<data-definition>` root node.
+  definitions = document.xpath('data-definition/*')
+
+  # <code-helper> tags have no use.
+  definitions.xpath('//code-helper').remove
+  definitions.xpath('//text()').each do |element|
+    element.remove if element.content.match?(/^\n+\s+$/)
+  end
+
   File.open("../dist/library/#{File.basename(xml).sub('.xml', '.lua')}", 'w') do |output|
     output.write("---THIS FILE WAS AUTOMATICALLY GENERATED. DO NOT EDIT.\n")
     output.write("---@meta #{File.basename(xml, '.xml')}\n\n")
-
-    # Provided files should have a single `<data-definition>` root node.
-    definitions = document.css('data-definition > *')
-
-    # <code-helper> tags have no use (yet)
-    definitions.xpath('//code-helper').remove
-    definitions.xpath('//text').remove
 
     # Write globals separately as they only exist in df.global.xml
     global_nodes = definitions.xpath('//global-object')
     output.write(GlobalObject.new(global_nodes).render) unless global_nodes.empty?
 
-    lua_annotations = {}
     definitions.each_with_index do |node, index|
       print "Writing definition ##{index}\r"
 
       case node.name
       when 'enum-type', 'bitfield-type'
-        parsed_node = EnumType.new(node)
-        lua_annotations[parsed_node.name] = parsed_node
+        output.write(EnumType.new(node).render)
       when 'struct-type', 'class-type', 'df-other-vectors-type'
-        parsed_node = StructType.new(node)
-        lua_annotations[parsed_node.name] = parsed_node
+        output.write(StructType.new(node).render)
       end
     end
-
-    lua_annotations.each { |_, annotation| output.write(annotation.render) }
   end
 end
