ed488f540a76e8f3523c98872e882814fa39ec84
vallode <18506096+vallode@users.noreply.github.com>
2024-03-30 17:53:06 +0100

    Adjust base classes

--- dist/library/base.lua
+++ dist/library/base.lua
@@ -1,13 +1,70 @@
----@meta
+---@meta _
 
----@alias df.kind
+---@alias DFKind
+---| 'primitive'
+---| 'struct'
+---| 'container'
+---| 'bitfield'
 ---| 'struct-type'
 ---| 'class-type'
 ---| 'enum-type'
 ---| 'bitfield-type'
 ---| 'global'
 
----@alias df.struct.mode
+-- Base class for all DFHack references and types.
+-- TODO: Better generics support once LuaLS gets better support:
+-- https://github.com/LuaLS/lua-language-server/issues/1861
+---@class DFBase
+---@field _kind DFKind
+local DFBase
+
+-- Object references
+---@class DFObject: DFBase
+---@field _type DFType
+local DFObject
+
+---@param self self
+---@return integer Size
+---@return integer? Address
+function DFObject:sizeof() end
+
+---@param self self
+---@return self
+function DFObject:new() end
+
+---@param self self
+---@return boolean
+function DFObject:delete() end
+
+---@param self self
+---@param object self|table
+function DFObject:assign(object) end
+
+---@param index integer
+---@param step? integer
+---@return self
+function DFObject:_displace(index, step) end
+
+-- Named types
+---@class DFType: DFBase
+---@field _identity lightuserdata
+local DFType
+
+---@param self self
+---@return integer Size
+---@return integer? Address
+function DFType:sizeof() end
+
+---@param self self
+---@return self
+function DFType:new() end
+
+---@param self self
+---@param object table
+---@return boolean
+function DFType:is_instance(object) end
+
+---@alias DFStructMode
 ---| 'END'
 ---| 'PRIMITIVE'
 ---| 'STATIC_STRING'
@@ -19,106 +76,53 @@
 ---| 'OBJ_METHOD'
 ---| 'CLASS_METHOD'
 
----@class df.compound.field
+-- Compound field
+---@class DFCompoundField: DFBase
 ---@field name string
 ---@field offset integer
 ---@field count integer
----@field mode df.struct.mode
+---@field mode DFStructMode
 ---@field type_name? string
----@field type? table
+---@field type? DFType
 ---@field type_identity? lightuserdata
----@field index_enum? table
----@field ref_target? table
+---@field index_enum? DFType
+---@field ref_target? DFType
 ---@field union_tag_field? string
 ---@field union_tag_attr? string
 ---@field original_name? string
+local DFCompoundField
 
--- TODO: Keep an eye out on the generics issue for Lua LSP
--- https://github.com/LuaLS/lua-language-server/issues/1861
----@class df.base
----@field _kind df.kind
----@field _identity userdata
-local dfbase
+-- Global object
+---@class DFGlobal: DFBase
+---@field _identity lightuserdata
+---@field _fields DFCompoundField[]
+local DFGlobal
 
----@param self self
----@return integer, integer
-function dfbase:sizeof() end
+-- Compound types
+---@class DFCompound: DFType
+---@class _fields DFCompoundField[]
+local DFCompound
 
----@param self self
----@return self
-function dfbase:new() end
-
----@param self self
----@return boolean
-function dfbase:delete() end
-
----@param self self
----@param object self|table
-function dfbase:assign(object) end
-
----@param index integer
----@param step? integer
----@return self
-function dfbase:_displace(index, step) end
-
----@class df.compound: df.base
----@field _fields df.compound.field[]
-
----@class df.class: df.compound
----@field is_instance fun(self: any, object: table): boolean|nil
-
----@class df.instance: df.class
----@field get_vector fun(): any[] The same as doing `df.global.<instance>.all`
-
----@class df.iter: df.base
----@field _first_item integer
----@field _last_item integer
-
----@class df.enum: df.iter
----@field _kind "enum-type"
-
----@class df.bitfield: df.iter
----@field _kind "bitfield-type"
-
--- TODO: Redo this as inline glue.
----@class df.container: df.base
----@field resize fun(self: self, new_size: integer)
----@field insert fun(self: self, index: "#ref"|"#"|integer, item: any)
----@field erase fun(self: self, index: integer)
-
----@class df.string: string
----@field value string
-
----@param object table
----@return integer
-function df.sizeof(object) end
+-- NULL value
+---@class NULL: lightuserdata
+NULL = nil
+df.NULL = NULL
 
 ---@param object any
----@return table
-function df.new(object) end
-
----@param object table
----@return boolean
-function df.delete(object) end
-
----@generic T
----@param object T
----@param object2 table
----@return T
-function df.assign(object, object2) end
-
---TODO: df.NULL and NULL
-
----@param object table
 ---@return boolean
 function df.isnull(object) end
 
----@param object table
+---@param object any
 ---@param allow_null? boolean
----@return boolean
+---@return string|NULL|nil
 function df.isvalid(object, allow_null) end
 
----@param type any
----@param object table
+---@param object any
+---@return integer|nil Size
+---@return integer|nil Address
+function df.sizeof(object) end
+
+---@param type DFType|DFObject
+---@param object DFObject
 ---@return boolean
 function df.is_instance(type, object) end
