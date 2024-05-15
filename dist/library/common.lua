6021c46b372e068b7ad2431af877225c32aa17b6
vallode <18506096+vallode@users.noreply.github.com>
2024-05-15 11:08:09 +0200

    Overload pen.make and pen.parse

--- dist/library/common.lua
+++ dist/library/common.lua
@@ -1,7 +1,11 @@
+-- Ideally this file would not be needed, here we put any methods/consts that
+-- need to be manually overloaded outside of base classes/types.
 ---@meta _
 
 ---@class df
 df = {}
+
+-- Core methods
 
 ---@param message string
 function dfhack.print(message) end
@@ -13,3 +17,22 @@ function dfhack.printerr(message) end
 ---@param level? integer
 ---@param verbose? boolean
 function dfhack.error(message, level, verbose) end
+
+-- Pen methods
+
+-- Creates a new `dfhack.pen` object.
+---@param base dfhack.pen|dfhack.color
+---@param pen_or_fg? dfhack.pen|dfhack.color
+---@param bg? dfhack.color
+---@param bold? boolean
+---@return dfhack.pen
+function dfhack.pen.make(base, pen_or_fg, bg, bold) end
+
+-- Returns `base` or `pen_or_fg` directly if they are already a valid
+-- `dfhack.pen` object.
+---@param base dfhack.pen|dfhack.color
+---@param pen_or_fg? dfhack.pen|dfhack.color
+---@param bg? dfhack.color
+---@param bold? boolean
+---@return dfhack.pen
+function dfhack.pen.parse(base, pen_or_fg, bg, bold) end
